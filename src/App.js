25c5d33a1a0b934245fbdabbff6f69558ee99d33
Arunava Ghosh <102473837+its-ag@users.noreply.github.com>
2022-10-12 11:31:18 +0530

    Added directory component & category component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,35 @@
-import './categories.styles.scss'
-
-const categories = [
-  {
-    id: 1,
-    title: 'Jackets',
-  },
-  {
-    id: 2,
-    title: 'Hats',
-  },
-  {
-    id: 3,
-    title: 'Sneakers',
-  },
-  {
-    id: 4,
-    title: 'Men',
-  },
-  {
-    id: 5,
-    title: 'Women',
-  },
-];
-
+import DirectoryItem from "./components/directory/directory.component";
 
 const App = () => {
-  return (
-  
-  <div className="categories-container">
-    {categories.map(({title, id}) => (
-       <div key={id} className="category-container">
-        <div className="background-image"/>
-       <div className="category-body-container">
-         <h2>{title}</h2>
-         <p>Shop Now</p>
-       </div>
-     </div>
-    ))}
-  </div>
-  );
-}
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+      imageUrl: "https://source.unsplash.com/random/?hats,fashion",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://source.unsplash.com/random/?jackets,fashion",
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://source.unsplash.com/random/?sneakers,fashion",
+    },
+    {
+      id: 4,
+      title: "women",
+      imageUrl: "https://source.unsplash.com/random/?women,fashion",
+    },
+    {
+      id: 5,
+      title: "men",
+      imageUrl: "https://source.unsplash.com/random/?men,fashion",
+    },
+  ];
+
+  return <DirectoryItem categories={categories} />;
+};
 
 export default App;
