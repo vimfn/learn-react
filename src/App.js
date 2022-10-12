98b1fedc0f8e24b4b2f6e1e94f43a76d3231fbcb
Arunava Ghosh <102473837+its-ag@users.noreply.github.com>
2022-10-12 09:22:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 const App = () => {
   return (
-  <div>
-  <h1>Hello AG</h1>
+  <div className="categories-container">
+    <div className="category-container">
+      {/* <img/> */}
+      <div className="category-body-container">
+        <h2>Jackets</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+    <div className="category-container">
+      {/* <img/> */}
+      <div className="category-body-container">
+        <h2>Hats</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+      <div className="category-container">
+      {/* <img/> */}
+      <div className="category-body-container">
+        <h2>Sneakers</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+    <div className="category-container">
+      {/* <img/> */}
+      <div className="category-body-container">
+        <h2>Men</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+    <div className="category-container">
+      {/* <img/> */}
+      <div className="category-body-container">
+        <h2>Women</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
   </div>
   );
 }
