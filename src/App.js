ce567b59aa9027bc94781fb638aeb92cf4216143
Arunava Ghosh <102473837+its-ag@users.noreply.github.com>
2022-10-12 10:05:27 +0530

    Added Scss
    Styling, added id

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import './categories.styles.scss'
+
 const categories = [
   {
     id: 1,
@@ -26,8 +28,8 @@ const App = () => {
   return (
   
   <div className="categories-container">
-    {categories.map(({title}) => (
-       <div className="category-container">
+    {categories.map(({title, id}) => (
+       <div key={id} className="category-container">
         <div className="background-image"/>
        <div className="category-body-container">
          <h2>{title}</h2>
